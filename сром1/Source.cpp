ecbf530233c0ec733b72d30b58eab373d4d0fe85
Maria <mmariia.koval@gmail.com>
2022-11-01 00:10:10 +0200

    karatsuba working!!!!!

--- сром1/Source.cpp
+++ сром1/Source.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <limits>
+#include <algorithm>
+
 using namespace std;
 #define ARCHITECTURE_TYPE 64
 #define MAX_UINT 18446744073709551615
@@ -26,8 +28,7 @@ private:
         int rem = 0;
         int i = 0;
         string hexNum(16, '0');
-        while (a != 0)
-        {
+        while (a != 0){
             rem = a % 16;
             if (rem < 10)
                 rem = rem + 48;
@@ -56,12 +57,10 @@ private:
     LongNum& addzero(int j) {
         uint64_t* temp;
         temp = new uint64_t[this->length + j];
-        for (int i = 0; i < this->length; i++) 
-        {
+        for (int i = 0; i < this->length; i++) {
             temp[i] = this->number[i];
         }
-        for (int i = this->length; i < this->length + j; i++) 
-        {
+        for (int i = this->length; i < this->length + j; i++) {
             temp[i] = 0;
         }
         delete[] this->number;
@@ -81,6 +80,18 @@ private:
         a.first.shrink_to_fit();
         a.second.shrink_to_fit();
         return a;
+    }
+    LongNum mult2pow(uint64_t pow) const {
+        LongNum temp = *this;
+        uint64_t c = pow / ARCHITECTURE_TYPE;
+        LongNum res((uint64_t)0, this->length + c);
+        for (int i = this->length - 1; i >= 0; i--) {
+            res.number[i + c] = this->number[i];
+        }
+        for (int i = 0; i < res.length - this->length - 1; i++) {
+            res.number[i] = 0;
+        }
+        return res;
     }
 public:
     LongNum() : LongNum((uint64_t)0, 1) {};
@@ -112,15 +123,19 @@ public:
         }
     }
     ~LongNum() {
-        delete[] number;
+        if (number) {
+            delete[] number;
+        }
     };
     void print() const {
+        cout << endl;
         for (int i = length - 1; i >= 0; i--) {
             cout << number[i] << " ";
         }
         cout << endl;
     }
     void printhex() const {
+        cout << endl;
         cout << this->gethex();
         cout << endl;
     }
@@ -132,8 +147,8 @@ public:
             std::reverse(tohex.begin(), tohex.end());
             temp = temp + tohex;
         }
-       // while (temp[0] == '0')
-          //  temp.erase(0, 1);
+       while (temp[0] == '0')
+         temp.erase(0, 1);
         return temp;
     }
     static LongNum readhex(string a) {
@@ -172,7 +187,7 @@ public:
         }
         while (c != 0 && i != max) {
             c = (sum.number[i] == MAX_UINT);
-            sum.number[i] = sum.number[i] + c;
+            sum.number[i] = sum.number[i] + 1;
             i++;
         }
         if (c != 0) {
@@ -207,7 +222,7 @@ public:
         }
         while (d != 0 && i != max) {
             d = (diff.number[i] == 0);
-            diff.number[i] = diff.number[i] - d;
+            diff.number[i] = diff.number[i] - 1;
             i++;
         }
         if (d != 0) {
@@ -226,7 +241,7 @@ public:
         }
         LongNum tilt;
         int k = 0;
-        if (this->length < 8) {
+        if (max < 8) {
             for (int i = 0; i < b.length; i++) {
                 int c = 0;
                 LongNum s((uint64_t)0, this->length + b.length);
@@ -243,6 +258,7 @@ public:
             }
             mult.shrink_to_fit();
             return mult;
+            
         }
         else {
             LongNum temp = *this;
@@ -250,48 +266,36 @@ public:
             if (max % 2 != 0) {
                 max = max + 1;
             }
+     
             pair<LongNum, LongNum> a = temp.kar_sep(max);
+            uint64_t s = (max / 2) * ARCHITECTURE_TYPE;
             a1 = a.first;
-            a1.print();
-            cout << endl;
             a0 = a.second;
-            a0.print();
-            cout << endl;
             a = b.kar_sep(max);
             b1 = a.first;
-            b1.print();
-            cout << endl;
             b0 = a.second;
-            b0.print();
-            cout << endl;
             c0 = b0 * a0;
-            c0.print();
-            cout << endl;
             c1 = a1 * b1;
-            c1.print();
-            cout << endl;
-            c2 = ((((a1 + a0) * (b1 + b0)) - c1) - c2);
-            c2.print();
-            cout << endl;
-            uint64_t s = (max / 2) * ARCHITECTURE_TYPE;
-            mult = (c1 >> (s*2)) + (c2 >> s) + c0;
-            return mult;
+            LongNum zxc = ((a1 + a0) * (b1 + b0));
+            c2 = ((zxc - c1) - c0);
+            c1.addzero(2*s / ARCHITECTURE_TYPE);
+            c2.addzero(s / ARCHITECTURE_TYPE);
+            mult = ((c1 << 2*s) + (c2<<s));
+            return mult + c0;
         }
     }
     LongNum operator<<(uint64_t s) const {
         if (s == 0) {
             return *this;
         }
-        uint64_t k, r;
+        int k, r;
         k = s / ARCHITECTURE_TYPE;
         r = s % ARCHITECTURE_TYPE;
         uint64_t carry = 0;
         uint64_t temp;
         LongNum res((uint64_t)0, this->length);
-        if (k != 0) {
-            for (int i = length - 1; i >= k; i--) {
-                res.number[i] = number[i - k];
-            }
+        for (int i = length - 1; i >= k; i--) {
+            res.number[i] = number[i - k];
         }
         if (r != 0) {
             for (int i = 0; i < length; i++) {
@@ -306,15 +310,13 @@ public:
         if (s == 0) {
             return *this;
         }
-        uint64_t k = s / ARCHITECTURE_TYPE;
-        uint64_t r = s % ARCHITECTURE_TYPE;
+        int k = s / ARCHITECTURE_TYPE;
+        int r = s % ARCHITECTURE_TYPE;
         uint64_t carry = 0;
         uint64_t temp;
         LongNum res((uint64_t)0, this->length);
-        if (k != 0) {
-            for (int i = 0; i < length - k ; i++) {
-                res.number[i] = number[i + k];
-            }
+        for (int i = 0; i < length - k ; i++) {
+            res.number[i] = number[i + k];
         }
         if (r != 0) {
             for (int i = length - 1; i > 0; i--) {
@@ -344,20 +346,19 @@ public:
     static LongNum mult_for64(uint64_t a, uint64_t b) {
         uint64_t a1, a2, b1, b2;
         LongNum res((uint64_t)0, 2);
-        LongNum c1((uint64_t)0, 1);
-        LongNum c2((uint64_t)0, 1);
+        LongNum c1((uint64_t)0, 2);
+        LongNum c2((uint64_t)0, 2);
         a1 = (a >> 32);
         a2 = ((a << 32) >> 32);
         b1 = (b >> 32);
         b2 = ((b << 32) >> 32);
         c1.number[0] = a1 * b2;
         c2.number[0] = a2 * b1;
-        LongNum s((uint64_t)0, 2);
-        s = c1 + c2;
-        s << 32;
+        LongNum s = (c1 + c2);
+        s = (s << 32);
         res.number[1] = (a1 * b1);
         res.number[0] = (a2 * b2);
-        LongNum res1 = res + s;
+        LongNum res1 = (res + s);
         return res1;
     }
 
@@ -375,45 +376,21 @@ public:
 };
 
 int main() {
-    /*
     string A = "D4D2110984907B5625309D956521BAB4157B8B1ECE04043249A3D379AC112E5B9AF44E721E148D88A942744CF56A06B92D28A0DB950FE4CED2B41A0BD38BCE7D0BE1055CF5DE38F2A588C2C9A79A75011058C320A7B661C6CE1C36C7D870758307E5D2CF07D9B6E8D529779B6B2910DD17B6766A7EFEE215A98CAC300F2827DB";
     string B = "3A7EF2554E8940FA9B93B2A5E822CC7BB262F4A14159E4318CAE3ABF5AEB1022EC6D01DEFAB48B528868679D649B445A753684C13F6C3ADBAB059D635A2882090FC166EA9F0AAACD16A062149E4A0952F7FAAB14A0E9D3CB0BE9200DBD3B0342496421826919148E617AF1DB66978B1FCD28F8408506B79979CCBCC7F7E5FDE7";
-    string C = "10F51035ED319BC50C0C4503B4D44872FC7DE7FC00F5DE863D6520E3906FC3E7E8761505118C918DB31AADBEA5A054B13A25F259CD47C1FAA7DB9B76F2DB450861BA26C4794E8E3BFBC2924DE45E47E5408536E3548A03591DA0556D595AB78C55149F45170F2CB7736A46976D1C09BFCE4DF6EAB040599AF235968F8070E25C2";
+    string C = "30A120B609DCBE28B09CA92E12DD29D77AE6400DC22B026AFB5FB945AAF62B57F4E48BD299261F02BBB35DD2495B5CD2713BF0E30192DAE1B334659160C8552423F0AD7FB82870920DF4E9B57980EAD2ADA9F3EF4B5D0718AB7F1053700395278998CB9AD48498D65150E3E837B0BB169D432B441424557061F838A17C65F90A31105F599BF69B87485BF9C70F51D37A417E476E372558C26782AC8C8F35C3D1227E851D8A72CD708700FD90C5E17F22C4EA15730345E56BD76F04B54580813CBE306B4404C6F34BCD9840D2911E6B3CF6DE3EE428C274EDF0A97335D8256DA26FCD67BA5450593A15F6B527ECE76FBBE20F7A882347614AF4B7FAF55086659D";
+    //std::transform(C.begin(), C.end(), C.begin(), [](char c) { return std::toupper(c); });
     LongNum AA = LongNum::readhex(A);
+    AA.print();
     LongNum BB = LongNum::readhex(B);
+    BB.print();
     LongNum CC = LongNum::readhex(C);
-    LongNum CCC = AA + BB;
+    LongNum CCC = AA * BB;
+    CCC.print();
     //AA.print()
     CC.printhex();
     cout << endl;
     CCC.printhex();
     cout << endl;
     cout << (CCC == CC) << endl;
-    */
-    uint64_t* number1 = new uint64_t[10];
-    uint64_t* number2 = new uint64_t[10];
-    for (int i = 0; i < 10; i++)
-    {
-        number1[i] = rand() % 100;
-    }
-    for (int i = 0; i < 10; i++)
-    {
-        number2[i] = rand() % 100;
-    }
-    LongNum a(number1, 10);
-    LongNum b(number2, 10);
-    a.print();
-    cout << endl;
-    a.printhex();
-    cout << endl;
-    b.print();
-    cout << endl;
-    b.printhex();
-    cout << endl;
-    LongNum c = a * b;
-    c.print();
-    cout << endl;
-    c.printhex();
-  
-
 }
